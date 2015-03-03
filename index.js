586ed95e56e9bfaf4f6dc73db5be72b312407878
Allen Vicencio <allenktv@gmail.com>
2015-03-02 19:31:35 -0800

    switching to sockets

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var express = require('express'),
-	app = express(),
+var http = require('http'),
+	app = require('express')(),
+	server = http.createServer(app),
+	io = require('socket.io').listen(server),
 	bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
 	routes = require('./routes/routes'),
 	constants = require('./common/constants/serverConstants');
+
+var usernames = [];
 
 mongoose.connect(constants.MONGO_IP, function (err) {
 	if (err) throw err;
@@ -15,7 +19,37 @@ mongoose.connect(constants.MONGO_IP, function (err) {
 
 	routes(app, mongoose.connection);
 
-	var server = app.listen(constants.PORT_NUMBER, function() {
-		console.log('Express server listening on port ' + server.address().port);
+	server.listen(constants.PORT_NUMBER, function() {
+		console.log('Express server listening on port ' + constants.PORT_NUMBER);
+	});
+
+	app.get('/', function (req, res) {
+  		res.sendFile(__dirname + '/test.html');
+	});
+
+	io.on('connection', function (socket) {
+		console.log('connection detected');
+		socket.on('send message', function (data, callback) {
+			console.log(socket.user.username + ' : ' + data);
+			io.sockets.emit('new message', {msg : data, username : socket.user.username});
+			if (callback) {
+				callback({code: 'success'});
+			}
+		});
+
+		socket.on('createUser', function (data, callback) {
+			console.dir(data);
+			//create user here
+			socket.user = data;
+			usernames.push(socket.user.username);
+			if (callback) {
+				callback(true);
+			}
+			io.sockets.emit('usernames', usernames);
+		});
+
+		socket.on('disconnect', function (data) {
+			//remove user
+		});
 	});
 });
